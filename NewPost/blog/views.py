62867e7b8219f52f78edb8e5edcf7ecbfb9ec0fc
Boateng Johnson <80701309+EliteBoatengJohnson@users.noreply.github.com>
2024-10-14 00:25:57 +0000

    new

--- NewPost/blog/views.py
+++ NewPost/blog/views.py
@@ -3,12 +3,32 @@ from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from .forms import EmailPostForm
-
+from django.core.mail import send_mail
 
 # Create your views here.
 def post_share(request, post_id):
-    # pass
-    pass
+    # Retrieve post by id
+    post = get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
+    sent = False
+    
+    if request.method =='POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fileds passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri( post.get_absolute_url())
+            subject =()
+    else:
+        form =EmailPostForm()
+    return render(
+        request, 'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
+    )
 
 def post_list(request):
     post_list = Post.published.all()
